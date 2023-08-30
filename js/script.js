053f519942cf8e2d912c308b7a209f601db126fd
FilipeHibrael <filipehibrael27@gmail.com>
2023-08-29 23:51:34 -0300

    slide section

---
+++ js/script.js
@@ -0,0 +1,4 @@
+import Slide from './slide.js';
+
+const slide = new Slide('.slide__image', '.slide__text', '.slide__label');
+slide.init();
---
+++ js/slide.js
@@ -0,0 +1,33 @@
+export default class Slide {
+  constructor(slideImages, slideTexts, slideLabels) {
+    this.slideImages = document.querySelectorAll(slideImages);
+    this.slideTexts = document.querySelectorAll(slideTexts);
+    this.slideLabels = document.querySelectorAll(slideLabels);
+
+    this.changeContent = this.changeContent.bind(this);
+  }
+
+  addActiveClass(item, itemList) {
+    itemList.forEach((item) => item.classList.remove('active'));
+    item.classList.add('active');
+  }
+
+  changeContent() {
+    for (let c = 0; c < 3; c++) {
+      setTimeout(() => {
+        this.addActiveClass(this.slideImages[c], this.slideImages);
+        this.addActiveClass(this.slideTexts[c], this.slideTexts);
+        this.addActiveClass(this.slideLabels[c], this.slideLabels);
+      }, 6000 * c);
+    }
+  }
+
+  changeContentLoop() {
+    this.changeContent();
+    setInterval(this.changeContent, 18000);
+  }
+
+  init() {
+    this.changeContentLoop();
+  }
+}
